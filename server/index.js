93c4a2d452ba4c23a6bf2a4fcca06828c7fdf8c3
aghanime92 <74283826+aghanime92@users.noreply.github.com>
2025-05-31 09:35:00 -0700

    Revert recent changes

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors({ origin: 'https://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
